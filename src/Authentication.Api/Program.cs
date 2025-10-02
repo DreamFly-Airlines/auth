c4890292cf5cf77c338580fac9ee55520a6ade3e
ArtemToporkov <toporkovartem259@gmail.com>
2025-10-02 12:29:19 +0500

    feat: user Swagger

--- src/Authentication.Api/Program.cs
+++ src/Authentication.Api/Program.cs
@@ -1,5 +1,9 @@
 using Authentication.Api.Extensions;
+using Authentication.Application.Commands;
+using Authentication.Application.Queries;
 using Authentication.Application.Services;
+using Authentication.Domain.Repositories;
+using Authentication.Infrastructure.Repositories;
 using Authentication.Infrastructure.Services;
 using Shared.Abstractions.Commands;
 using Shared.Abstractions.Queries;
@@ -13,12 +17,19 @@ builder.Services.AddScoped<ICommandSender, ServiceProviderCommandSender>();
 builder.Services.AddScoped<IQuerySender, ServiceProviderQuerySender>();
 builder.Services.AddSingleton<IPasswordHasherService, BCryptPasswordHasherService>();
 builder.Services.AddSingleton<IJwtProviderService, MicrosoftIdentityModelJwtProviderService>();
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
 builder.Services.AddJwtOptions(builder.Configuration);
+builder.Services.AddCommandHandlers(typeof(RegisterCommandHandler).Assembly);
+builder.Services.AddQueryHandlers(typeof(LogInQueryHandler).Assembly);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapControllers();
